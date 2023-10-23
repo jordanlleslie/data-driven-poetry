0ec5e91a56bda963d71badeddbd3722abafe0e8e
Jordan Leslie <jordan02leslie@gmail.com>
2023-10-23 11:00:04 -0400

    progress on numeracy plot

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 let chart, chartWidth, chartHeight;
+let xScale, yScale;
 
 const svgWidth = 500;
 const svgHeight = 400;
 
-let xScale, yScale;
+let discrimination_experiences;
 
 let svg = d3.select("#svg");
 
 async function loadData() {
-  return;
+  discrimination_experiences = await d3.csv(
+    "/assets/data/discrimination_experiences.csv"
+  );
 }
 
 function initializeSVG() {
-  svg.attr("width", svgWidth);
-  svg.attr("height", svgHeight);
+  svg
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("overflow", "visible");
   svg.selectAll("*").remove();
 
-  const margin = { top: 40, right: 30, bottom: 50, left: 50 };
+  const margin = { top: 40, right: 30, bottom: 50, left: 30 };
   chartWidth = svgWidth - margin.left - margin.right;
   chartHeight = svgHeight - margin.top - margin.bottom;
 
@@ -45,36 +50,89 @@ function updateTitle(title) {
   }
 }
 
+function singleNumeracyChart(statement) {
+  svg.selectAll(".discrimination-icon").remove();
+  svg.select("#discrimination-statement").text(statement);
+
+  let row = discrimination_experiences.filter(
+    (entry) => entry["Experiences of discrimination"] === statement
+  );
+
+  //  if filter returns no rows, something went wrong
+  if (!row[0]) throw Error("No matching statement...something went wrong");
+  row = row[0];
+
+  console.log(row);
+  for (key of ["All", "Low prestige", "High prestige"]) {
+    value = parseInt(row[key]);
+    // TODO: Adjust position for different categories so we can compare them
+    // TODO: Figure out how to change fill color of SVG
+    for (i = 0; i < value; i++) {
+      chart
+        .append("image")
+        .attr("xlink:href", "/assets/images/Person.svg")
+        .attr("width", "15px")
+        .attr("class", "discrimination-icon")
+        .style("fill", "red")
+        .attr("x", (i % 10) * 15)
+        .attr("y", Math.floor(i / 10) * 15);
+    }
+  }
+}
+
 function makeNumeracyChart() {
   // implement interactive element (statement with forward and backward buttons)
 
-  const fontSize = 18;
+  const fontSize = 25;
+  const padding = fontSize / 2;
 
+  // get discrimination statements from data
+  const statements = discrimination_experiences.map(
+    (entry) => entry["Experiences of discrimination"]
+  );
+  console.log(statements);
+  //   start with first statement
+  let statementIndex = 0;
+
+  // statement
   chart
     .append("text")
     .attr("id", "discrimination-statement")
-    .attr("x", svgWidth / 2)
-    .text("Statement")
+    .attr("x", chartWidth / 2)
+    .text(statements[statementIndex])
+    .attr("text-anchor", "middle")
     .style("font-size", `${fontSize}px`);
 
-  chart
+  singleNumeracyChart(statements[statementIndex]);
+
+  // prev, next buttons
+  svg
     .append("image")
     .attr("xlink:href", "/assets/images/Right.svg")
     .attr("width", fontSize)
-    .attr("id", "next-statement")
+    .attr("id", "next-statement-btn")
+    .attr("x", svgWidth - padding * 3)
+    .attr("y", padding)
     // Click listener
     .on("click", () => {
-      //   NEXT STATEMENT
+      if (statementIndex < statements.length - 1) {
+        statementIndex++;
+        singleNumeracyChart(statements[statementIndex]);
+      }
     });
-
-  chart
+  svg
     .append("image")
     .attr("xlink:href", "/assets/images/Left.svg")
     .attr("width", fontSize)
-    .attr("id", "next-statement")
+    .attr("id", "prev-statement-btn")
+    .attr("x", padding)
+    .attr("y", padding)
     // Click listener
     .on("click", () => {
-      //   NEXT STATEMENT
+      if (statementIndex > 0) {
+        statementIndex--;
+        singleNumeracyChart(statements[statementIndex]);
+      }
     });
 
   // implement chart
