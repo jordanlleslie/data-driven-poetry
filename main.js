57529f05977032764bbee051ed0e4ff4000d76fc
Jordan Leslie <jordan02leslie@gmail.com>
2023-11-03 13:24:02 -0400

    scrolling done

--- main.js
+++ main.js
@@ -8,6 +8,115 @@ let svg = d3.select("#svg");
 
 let discrimination_experiences, correction_reasons;
 
+let keyframeIndex = 0;
+let keyframes = [
+  {
+    activeVerse: 1,
+    activeLines: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  },
+  {
+    activeVerse: 2,
+    activeLines: [1, 2, 3, 4],
+  },
+  {
+    activeVerse: 2,
+    activeLines: [5, 6, 7, 8, 9, 10, 11],
+    svgUpdate: numeracyChartWrapper,
+  },
+  {
+    activeVerse: 3,
+    activeLines: [1, 2, 3, 4, 5, 6],
+    svgUpdate: pieChartWrapper,
+  },
+  {
+    activeVerse: 4,
+    activeLines: [1, 2, 3, 4, 5, 6, 7, 8],
+  },
+];
+document.getElementById("backward-button").disabled = keyframeIndex === 0;
+document.getElementById("forward-button").disabled =
+  keyframeIndex === keyframes.length - 1;
+
+document
+  .getElementById("forward-button")
+  .addEventListener("click", forwardClicked);
+document
+  .getElementById("backward-button")
+  .addEventListener("click", backwardClicked);
+
+function forwardClicked() {
+  // Make sure we don't let the keyframeIndex go out of range
+  if (keyframeIndex < keyframes.length - 1) {
+    keyframeIndex++;
+    drawKeyframe(keyframeIndex);
+  }
+}
+
+function backwardClicked() {
+  if (keyframeIndex > 0) {
+    keyframeIndex--;
+    drawKeyframe(keyframeIndex);
+  }
+}
+
+function drawKeyframe(kfi) {
+  let kf = keyframes[kfi];
+  // update enabled/disabled buttons
+  document.getElementById("backward-button").disabled = kfi === 0;
+  document.getElementById("forward-button").disabled =
+    kfi === keyframes.length - 1;
+
+  resetActiveLines();
+  updateActiveVerse(kf.activeVerse);
+
+  for (let line of kf.activeLines) {
+    updateActiveLine(kf.activeVerse, line);
+  }
+
+  if (kf.svgUpdate) {
+    kf.svgUpdate();
+  }
+}
+
+function resetActiveLines() {
+  d3.selectAll(".line").classed("active-line", false);
+}
+
+function updateActiveVerse(id) {
+  d3.selectAll(".verse").classed("active-verse", false);
+  d3.select("#verse" + id).classed("active-verse", true);
+  scrollLeftColumnToActiveVerse(id);
+}
+
+function updateActiveLine(vid, lid) {
+  let thisVerse = d3.select("#verse" + vid);
+  thisVerse.select("#line" + lid).classed("active-line", true);
+}
+
+function scrollLeftColumnToActiveVerse(id) {
+  // select the div displaying the left column content
+  var poemContainer = document.querySelector("#poem");
+
+  // select the verse we want to display
+  var activeVerse = document.getElementById("verse" + id);
+
+  // calculate the bounding rectangles of both of these elements
+  var verseRect = activeVerse.getBoundingClientRect();
+  var poemRect = poemContainer.getBoundingClientRect();
+
+  // calculate the desired scroll position
+  var desiredScrollTop =
+    verseRect.top +
+    poemContainer.scrollTop -
+    poemRect.top -
+    (poemRect.height - verseRect.height) / 2;
+
+  poemContainer.scrollTo({
+    top: desiredScrollTop,
+    behavior: "smooth",
+  });
+}
+
 async function loadData() {
   discrimination_experiences = await d3.csv(
     "./assets/data/discrimination_experiences.csv"
